4c147727f2033eafc3e07adea83ddf7006efcb0b
Lennon Shimokawa <lshimokawa@gmail.com>
2011-09-29 09:49:19 -0500

    haciendo el sorteo por get

--- twit_raffle.rb
+++ twit_raffle.rb
@@ -7,7 +7,7 @@ get '/' do
   erb :index
 end
 
-post '/' do
+get '/raffle' do
   @users = []
   Twitter::Search.new.hashtag(params[:hashtag]).per_page(100).each do |tweet|
     @users << tweet.from_user unless @users.include? tweet.from_user
