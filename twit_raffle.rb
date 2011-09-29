0bb3dc711eba7b0d7890c09f1d3902767e52ce86
Xenda <xendaccount@xendacentral.com>
2011-09-29 06:50:14 -0500

    Edited twit_raffle.rb via GitHub

--- twit_raffle.rb
+++ twit_raffle.rb
@@ -8,12 +8,11 @@ get '/' do
 end
 
 post '/' do
-  users = Array.new
+  @users = []
   Twitter::Search.new.hashtag(params[:hashtag]).per_page(100).each do |tweet|
     users << tweet.from_user unless users.include? tweet.from_user
   end
-  @users_list = users.join ', '
-  @winner = users[rand(users.size)]
   
+  @winner = users.shuffle.first
   erb :index
 end
